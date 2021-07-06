591cf43ae1e4684931d9f378c7d58d46db2bce1b
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-05 20:58:30 -0500

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,12 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
+
 
 const app = express();
 
 const PORT = 3000;
 
+//mongo connection 
+mongoose.Promise = global.Promise;
+//to connect the mongondb to the cloud 
+mongoose.connect('mongodb://localhost/soccerDB', {
+    userNewUrlParser: true,
+    useUnifiedTopology:true,
+
+});
+//body parse for the setup
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
 app.get('/', (req, res)=> 
-res.send(`our soccer application is running ${PORT}`)
+res.send(`our soccer application is running 3000 ${PORT}`)
 );
 
-app.listen(PORT,()=> 
+app.listen(PORT,() => 
 console.log(`your soccer server is running on port ${PORT}`));
