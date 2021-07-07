e8ee18ce422c63693908ecd90c606dfc3aa6f72d
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-06 20:21:13 -0500

    Add files via upload

---
+++ BackEnd/playerControllers.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { PlayerSchema } from '../models/playerModel';
+
+const Player = mongoose.model('player', PlayerSchema);
+
+export const addNewPlayer = (req,res) => {
+    let newPlayer = new Player(req.body);
+
+    newPlayer.save((err,Player) => {
+        if(err){
+            res.send(err)
+            
+        }
+        res.json(Player);
+    });
+};
+
