51795784c1d4366acf79c6334741fb9d6406c0b2
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-11 15:36:13 -0500

    Add files via upload

--- BackEnd/playerControllers.js
+++ BackEnd/playerControllers.js
@@ -3,15 +3,51 @@ import { PlayerSchema } from '../models/playerModel';
 
 const Player = mongoose.model('player', PlayerSchema);
 
-export const addNewPlayer = (req,res) => {
+export const addNewPlayer = (req, res) => {
     const newPlayer = new Player(req.body);
 
     newPlayer.save((err,Player) => {
-        if(err){
-            res.send(err)
-            
+        if(err) {
+            res.send(err)  
         }
         res.json(Player);
     });
 };
+// the get request  for the player
+export const getPlayers = (req, res) => {
+    Player.find({},(err,Player) => {
+        if(err) {
+            res.send(err)  
+        }
+        res.json(Player);
+    });
+};
+//to get the id of the player.
+export const getPlayerWithId = (req,res) => {
+    Player.findById(req.params.PlayerId,(err,Player) => {
+        if(err){
+            res.send(err)
+        }
+        res.json(Player)
+    });
+}
+//Put method for the player
+export const UpdatePlayer = (req,res) => {
+    Player.findOneAndUpdate({_id: req.params.PlayerId},req.body,{new:true},(err,Player) => {
 
+        if(err){
+            res.send(err)
+        }
+        res.json(Player)
+    });
+}
+
+//to get the id of the player.
+export const delatePlayer = (req,res) => {
+    Player.remove({_id: req.params.PlayerId},(err,Player) => {
+        if(err){
+            res.send(err)
+        }
+        res.json(message(`successfully delated player`))
+    });
+}
