d98a709c506d9a8d9cf4416e70515110830c9978
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-20 21:43:43 -0500

    Add files via upload

--- BackEnd/playerControllers.js
+++ BackEnd/playerControllers.js
@@ -14,15 +14,16 @@ export const addNewPlayer = (req, res) => {
     });
 };
 // the get request  for the player
+// check the req to the database
 export const getPlayers = (req, res) => {
-    Player.find({},(err,Player) => {
+    Player.find({},(err, Player) => {
         if(err) {
             res.send(err)  
         }
         res.json(Player);
     });
 };
-//to get the id of the player.
+//Get the id of the player.
 export const getPlayerWithId = (req,res) => {
     Player.findById(req.params.PlayerId,(err,Player) => {
         if(err){
@@ -33,7 +34,7 @@ export const getPlayerWithId = (req,res) => {
 }
 //Put method for the player
 export const UpdatePlayer = (req,res) => {
-    Player.findOneAndUpdate({_id: req.params.PlayerId},req.body,{new:true},(err,Player) => {
+    Player.findOneAndUpdate({ _id: req.params.PlayerId},req.body,{new:true},(err,Player) => {
 
         if(err){
             res.send(err)
@@ -42,9 +43,9 @@ export const UpdatePlayer = (req,res) => {
     });
 }
 
-//to get the id of the player.
+//to get the id of a specific player
 export const delatePlayer = (req,res) => {
-    Player.remove({_id: req.params.PlayerId},(err,Player) => {
+    Player.remove({ _id: req.params.PlayerId},(err,Player) => {
         if(err){
             res.send(err)
         }
