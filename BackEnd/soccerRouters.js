b074ce9dfc47a6a68a5793fc143f46421c3980ad
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-20 21:44:28 -0500

    Add files via upload

--- BackEnd/soccerRouters.js
+++ BackEnd/soccerRouters.js
@@ -1,10 +1,21 @@
-import { addNewPlayer } from '../controller/playerControllers';
+import { addNewPlayer,delatePlayer,getPlayers
+    , getPlayerWithId
+    , UpdatePlayer} from '../controller/playerControllers';
 
 const routes = (app) => {
     app.route('players')
-
+    //router getPlayers
+    .get(getPlayers)
     //Post endpoint this is the post end point
     .post(addNewPlayer);
+    //new route for the IDplayer 
+    app.route('/player/:PlayerId')
+    //this is to get an specific player
+    .get(getPlayerWithId)
+    //this is to get an specific player
+    .put(UpdatePlayer)
+    //for the delate of the player
+    .delate(delatePlayer)
 }
 
 export default routes;
