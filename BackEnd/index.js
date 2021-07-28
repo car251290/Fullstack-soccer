aa3c2050b1f3f70e06d2142d85f63d46c7b25e19
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-07-27 20:55:16 -0500

    Add files via upload

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,27 +1,31 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
-
+import routers from './routes/soccerRouters';
+import cors from 'cors'
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 4000;
 
 //mongo connection 
 mongoose.Promise = global.Promise;
 //to connect the mongondb to the cloud 
 mongoose.connect('mongodb://localhost/soccerDB', {
     userNewUrlParser: true,
-    useUnifiedTopology:true,
+    useUnifiedTopology: true,
 
 });
 //body parse for the setup
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
-
+//CORS setup
+app.use(cors());
+routers(app);
+// index for get
 app.get('/', (req, res)=> 
-res.send(`our soccer application is running 3000 ${PORT}`)
+res.send(`our soccer application is running 4000 ${PORT}`)
 );
-
+// Listening of the PORT = URL is the local host 4000
 app.listen(PORT,() => 
 console.log(`your soccer server is running on port ${PORT}`));
