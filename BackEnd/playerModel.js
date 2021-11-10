d595fa472df417cf71bbd999f7ccd543e0dcb6af
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-11-10 17:49:07 -0600

    Add files via upload

--- BackEnd/playerModel.js
+++ BackEnd/playerModel.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const PlayerSchema = new Schema({
-    //objects for display the data
     firstName: {
         type: String,
         required: true,
